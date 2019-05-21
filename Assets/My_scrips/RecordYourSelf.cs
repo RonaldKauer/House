59cd56a7b54a26f4f61d8cfa56b748925ab8b0b0
DESKTOP-AT3O5T2\RonBon <ron.kauer@gmail.com>
2019-05-21 18:28:03 -0400

    first comit
    adding project to github

---
+++ Assets/My_scrips/RecordYourSelf.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class RecordYourSelf : MonoBehaviour
+{
+    // Start is called before the first frame update
+     // Get list of Microphone devices and print the names to the log
+    public AudioSource testSound;
+		// Material mat;
+		
+
+    void Start(){
+			testSound = GetComponent<AudioSource>();
+		// 	MeshRenderer mesh = GetComponent<MeshRenderer>();
+		//  	Material oldMaterial = mesh.material;
+		// // testSound.Play();
+    }
+
+	void OnMouseDown(){
+		testSound.Play();
+	}
+
+	// void OnMouseEnter(){
+	// 	mat = Resources.Load<Material>("Glow");
+	// 	MeshRenderer mesh = GetComponent<MeshRenderer>();
+	// 	Material oldMaterial = mesh.material;
+	// 	mesh.materials[1] = mat;
+	// }
+
+	// void OnMouseExit(){
+	// 	mesh.material = oldMaterial;
+	// }
+
+}
