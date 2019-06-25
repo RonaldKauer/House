700d1c56d3832fdca39d0f91fee8344387c53196
Ron Kauer <31354827+RonaldKauer@users.noreply.github.com>
2019-06-25 14:27:07 -0400

    button
    got buttons working

---
+++ House_build/Assets/My_scrips/Close_button.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Close_button : MonoBehaviour
+{
+ 
+    public GameObject canvasObj;
+    // public GameObject canvasObj2;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Close_button")
+        {
+            canvasObj.SetActive(false);
+            // canvasObj2.SetActive(false);
+
+        }
+    }
+
+}
--- House_build/Assets/My_scrips/RecordYourSelf.cs
+++ House_build/Assets/My_scrips/RecordYourSelf.cs
@@ -5,6 +5,8 @@ using UnityEngine.Audio;
 
 public class RecordYourSelf : MonoBehaviour
 {
+   public GameObject canvasObj;
+//    public GameObject canvasObj2;
     // Start is called before the first frame update
      // Get list of Microphone devices and print the names to the log
     public AudioSource testSound;
@@ -13,24 +15,36 @@ public class RecordYourSelf : MonoBehaviour
 
     void Start(){
 			testSound = GetComponent<AudioSource>();
+			//canvasObj = transform.Find("Chair_Menu").gameObject as GameObject;
+			// canvasObj.SetActive(false);
 		// 	MeshRenderer mesh = GetComponent<MeshRenderer>();
 		//  	Material oldMaterial = mesh.material;
 		// // testSound.Play();
     }
 
-	void OnMouseDown(){
-		testSound.Play();
-	}
 
-	// void OnMouseEnter(){
-	// 	mat = Resources.Load<Material>("Glow");
-	// 	MeshRenderer mesh = GetComponent<MeshRenderer>();
-	// 	Material oldMaterial = mesh.material;
-	// 	mesh.materials[1] = mat;
+	// void OnMouseDown(){
+	// 	testSound.Play();
+	// 	canvasObj.SetActive(true);
 	// }
 
-	// void OnMouseExit(){
-	// 	mesh.material = oldMaterial;
+	// void Update(){
+	// 	if((OVRInput.Get (OVRInput.Button.Two))){
+	// 		canvasObj.SetActive(true);
+	// 		testSound.Play();
+	// 	}
 	// }
+	
+
+	void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Hand" )
+        {
+            // testSound.Play();
+			canvasObj.SetActive(true);
+			// canvasObj2.SetActive(true);
+        }
+    }
+
 
 }
