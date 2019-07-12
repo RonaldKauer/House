3a619e82d0de16514cf41fb813cc10786e6db86e
DESKTOP-AT3O5T2\RonBon <ron.kauer@gmail.com>
2019-07-12 18:56:01 -0400

    new design
    added more stuff

---
+++ House_build/Assets/My_scrips/Tset.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Linq;
+using UnityEngine.Windows.Speech;
+
+public class Tset : MonoBehaviour
+{
+    public static Tset tset = null;
+    private KeywordRecognizer keywordRecognizer;
+    private Dictionary<string, Action> actions = new Dictionary<string, Action>();
+    public AudioSource testSound;
+    private bool yes;
+    // actions.Add("la mesa", mesa);
+    // actions.Add("okay", okay);
+    void Awake(){
+        if (tset == null){
+            tset = this;
+        }
+        else{
+            Destroy(gameObject);
+        }
+    }
+    void Start(){
+			testSound = GetComponent<AudioSource>();
+    }
+    // Start is called before the first frame update
+    void OnMouseDown()
+    {
+        actions.Add("la silla", mesa);
+        actions.Add("okay", okay);
+        yes = true;
+
+        keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());   
+        keywordRecognizer.OnPhraseRecognized += RecognizedSpeech; 
+        keywordRecognizer.Start();
+    }
+
+    private void RecognizedSpeech(PhraseRecognizedEventArgs speech){
+        Debug.Log(speech.text);
+        actions[speech.text].Invoke();
+    }
+
+    private void mesa(){
+        Debug.Log("you said la silla");
+        if (yes == true){
+            testSound.Play();
+        }
+        yes = false;
+    }
+
+    private void okay(){
+        Debug.Log("you said okay");
+    }
+
+    private void OnDisable(){
+        keywordRecognizer.OnPhraseRecognized -= RecognizedSpeech;
+    }
+}
---
+++ House_build/Assets/My_scrips/Tutorial_button.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using VRStandardAssets.Utils;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Tutorial_button : MonoBehaviour
+{
+	[SerializeField] private Material m_NormalMaterial;                
+	[SerializeField] private Material m_OverMaterial;                  
+	[SerializeField] private Material m_ClickedMaterial;               
+	// [SerializeField] private Material m_DoubleClickedMaterial;         
+	[SerializeField] private VRInteractiveItem m_InteractiveItem;
+	[SerializeField] private Renderer m_Renderer;
+
+
+
+	private void Awake ()
+	{
+		m_Renderer.material = m_NormalMaterial;
+	}
+
+
+	private void OnEnable()
+	{
+		m_InteractiveItem.OnOver += HandleOver;
+		m_InteractiveItem.OnOut += HandleOut;
+		m_InteractiveItem.OnClick += HandleClick;
+		// m_InteractiveItem.OnDoubleClick += HandleDoubleClick;
+	}
+
+
+	private void OnDisable()
+	{
+		m_InteractiveItem.OnOver -= HandleOver;
+		m_InteractiveItem.OnOut -= HandleOut;
+		m_InteractiveItem.OnClick -= HandleClick;
+		// m_InteractiveItem.OnDoubleClick -= HandleDoubleClick;
+	}
+
+
+	//Handle the Over event
+	private void HandleOver()
+	{
+		Debug.Log("Show over state");
+		m_Renderer.material = m_OverMaterial;
+
+	}
+
+
+	//Handle the Out event
+	private void HandleOut()
+	{
+		Debug.Log("Show out state");
+		m_Renderer.material = m_NormalMaterial;
+
+
+	}
+
+
+	//Handle the Click event
+	private void HandleClick()
+	{
+		Debug.Log("IM CLICKING PLS");
+		SceneManager.LoadScene(1);
+	}
+		
+	// //Handle the DoubleClick event
+	// private void HandleDoubleClick()
+	// {
+
+	// 	// Create a temporary reference to the current scene.
+	// 	Scene currentScene = SceneManager.GetActiveScene ();
+
+	// 	// Retrieve the name of this scene.
+	// 	string sceneName = currentScene.name;
+
+	// 	Debug.Log("Show double click");
+	// 	m_Renderer.material = m_NormalMaterial;
+	// 	// m_Renderer.material = m_DoubleClickedMaterial;
+
+	// 	if (sceneName == "World") {
+	// 		itemText.SetActive (false);
+	// 	}
+	// }
+}
