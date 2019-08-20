73a579e7449003e6f249dfb9f0e737742eae3bcf
DESKTOP-AT3O5T2\RonBon <ron.kauer@gmail.com>
2019-08-20 01:27:43 -0400

    added all items
    all item done

--- House_build/Assets/My_scrips/ScavengerHunt.cs
+++ House_build/Assets/My_scrips/ScavengerHunt.cs
@@ -8,31 +8,28 @@ public class ScavengerHunt : MonoBehaviour
 {
 	public static ScavengerHunt sca = null;
 	protected static int tracker = 0;
+
+	public AudioClip[] HuntItemsAudioClips; //the audio fro the hunt items
+	public AudioClip[] HuntQuestionsAudioClips; // the auido for hunt "donde esta.."
+	public GameObject[] HuntItems; // the items for the hunt
+	public string[] HuntPrompts; // theh string that display "donde esta" for the hunt
+	public List<int> checking = new List<int>();
+
 	public AudioClip[] itemsAudioClips; //the audio fro the items
 	public AudioClip[] questionsAudioClips; // the auido "donde esta.."
 	public GameObject[] Items; // the items
 	public string[] Prompts; // theh string that display "donde esta"
 
-	public AudioClip[] HuntitemsAudioClips; //the audio fro the hunt items
-	public AudioClip[] HuntquestionsAudioClips; // the auido for hunt "donde esta.."
-	public GameObject[] HuntItems; // the items for the hunt
-	public string[] HuntPrompts; // theh string that display "donde esta" for the hunt
-	public List<int> checking = new List<int>();
-
     public int min = 0;
-    public int max = 1;
+    public int max = 28;
 	public System.Random _rnd = new System.Random();
 
 	public Text  _thetraker;
 	public Text _thePromts;
-	// public Text touchScoreText;
-	// public Text touchPromptText;
-	// public Text ScoreText;
-	// public Text PromptText;
 	public AudioSource A;
 	public List<AudioSource> aud;
     public Text endText;
-    int EndOfTextCount = 0;
+
     private int i = 0;
 
 	void Awake()
@@ -48,13 +45,13 @@ public class ScavengerHunt : MonoBehaviour
     void Start(){
 		A = GetComponent<AudioSource> ();
 
-        HuntitemsAudioClips = new AudioClip[10];
-        HuntquestionsAudioClips = new AudioClip[10];
+        HuntItemsAudioClips = new AudioClip[10];
+        HuntQuestionsAudioClips = new AudioClip[10];
         HuntItems = new GameObject[10];
         HuntPrompts = new string[10];
         RandTheHunt();
 
-		A.clip = HuntquestionsAudioClips [tracker];
+		A.clip = HuntQuestionsAudioClips[tracker];
 		A.Play ();
 
 		updateText();
@@ -63,18 +60,20 @@ public class ScavengerHunt : MonoBehaviour
 
     public void RandTheHunt(){
         //i have 28 items to choose from (need to be 30)
-        for(int i = 0; i < 2; ++i){
-            int num = _rnd.Next(min, max+1);
+        for(int j = 0; j < 10; ++j){
+            int num = _rnd.Next(min, max);
+			Debug.Log(num);
 			if(!checking.Contains(num)){
 				checking.Add(num);
 				// gwtting from the overall array into the hunt items array fro the actual hunt
-				HuntItems[i] = Items[num];
-				HuntitemsAudioClips[i] = itemsAudioClips[num];
-				HuntquestionsAudioClips[i] = questionsAudioClips[num];
-				HuntPrompts[i] = Prompts[num];
+				HuntItems[j] = Items[num];
+				HuntItemsAudioClips[j] = itemsAudioClips[num];
+				HuntQuestionsAudioClips[j] = questionsAudioClips[num];
+				HuntPrompts[j] = Prompts[num];
 			}
 			else{
-				--i;
+				Debug.Log("num was already in");
+				--j;
 			}
         }
     }
@@ -98,10 +97,6 @@ public class ScavengerHunt : MonoBehaviour
         Debug.Log("End ");
 		_thetraker.text = tracker.ToString() + "/10 itesm found";
 		_thePromts.text = HuntPrompts[tracker].ToString();
-        // touchScoreText.text = tracker.ToString() + "/15 items found";
-		// touchPromptText.text = Prompts [tracker].ToString ();
-		// ScoreText.text = tracker.ToString()+ "/15 items found";
-		// PromptText.text = Prompts[tracker].ToString();
         //endText.text = "Thank you for playing";
         
         if (tracker == (aud.Count - 1))
@@ -116,23 +111,23 @@ public class ScavengerHunt : MonoBehaviour
 	}
 
 	public GameObject getObject(){
-		return Items [tracker];
+		return HuntItems [tracker];
 	}
 
 	public void callRoutineAndIncrementTracker(){
 		Debug.Log (tracker);
-		if (tracker < HuntitemsAudioClips.Length) {
+		if (tracker < HuntItemsAudioClips.Length) {
 			StartCoroutine(PlayAudio ());
 		}
 	}
 
 	IEnumerator PlayAudio(){
 		Debug.Log ("PlayClip");
-		A.clip = HuntitemsAudioClips [tracker];
+		A.clip = HuntItemsAudioClips [tracker];
 		A.Play ();
 		tracker++;
 		yield return new WaitForSeconds (A.clip.length + 1);
-		A.clip = HuntquestionsAudioClips [tracker];
+		A.clip = HuntQuestionsAudioClips [tracker];
 		A.Play ();
 	}
 
